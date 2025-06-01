3f9bb3f490144fee9c4749977c9dff69d35f71c6
RockingWithTheBest <151398987+RockingWithTheBest@users.noreply.github.com>
2025-06-01 17:07:40 +0300

    feat: added an addOrder method and also added certain text fields

--- User Interface/src/Components/SuppliersJob/Linking/Linking.jsx
+++ User Interface/src/Components/SuppliersJob/Linking/Linking.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import DataTable from "react-data-table-component";
 import { useState, useEffect} from 'react';
-    import Popup from 'reactjs-popup';
+import TextField from '@mui/material/TextField';
+import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 import GenerateProduct from '../GenerateProduct/GenerateProduct';
 import  Logistics  from '../Logistics/Logistics';
-import './Linking.css'
+
 import Button from '@mui/material/Button';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-
+import './Linking.css'
 
 
 
@@ -39,6 +40,7 @@ const columns =(onGetProduct)=> [
     },
     {
         name: 'Product Id',
+        width: '140px',
         selector:row=>row.ProductId,
         sortable: true
     },
@@ -69,6 +71,12 @@ const OrderLink = () =>{
     const [records, setRecordOfOrders] = useState([]);
     const [selectedProductId, setSelectedProductId] = useState(null);
     const [open, setOpen] = useState(false);
+    const [openOrderPop, setOpenOrderPop] = useState(false)
+    const [orderstatus, setOrderStatus] =  useState("")
+    const [taxstatus, setTaxStatus] =  useState("")
+    const [quantity, setQuantity] =  useState("")
+    const [productId, setProductId] =  useState("")
+    const [clientId, setClientID] =  useState("")
 
 
     const API_URL = 'https://localhost:7136/';
@@ -93,11 +101,48 @@ const OrderLink = () =>{
         setOpen(true);
         console.log(productId);
     }
+    const PostOrder = ()=>{
+        const newOrder =   {
+            orderStatus: orderstatus,
+            taxStatus: taxstatus,
+            quantity: quantity,
+            productId: productId,
+            customerId: clientId           
+          }
+          console.log("PPPP",newOrder)
+        if(newOrder.customerId === '' || 
+            newOrder.orderStatus===''|| newOrder.taxStatus===''||
+            newOrder.quantity===''||newOrder.productId===''){
+            alert("Поля не заполнены")
+        }
+        else{
+            try{
+                fetch(`${API_URL}api/Order/Add-Order`,
+                    {
+                        method: 'POST',
+                        headers:{
+                            'Content-Type':'application/json'
+                        },
+                        body:JSON.stringify(newOrder)
+                    }
+                )
+                refreshItems(); 
+                alert("Успешно добавлено")
+            }
+            catch(error){
+                console.error('Error posting data:', error);
+            }        
+        }    
+    }
+    const addOrder = ()=>{
+        setOpenOrderPop(true)
+         refreshItems();  
+    }
     return (
         <div className='DataLog'>
            <div className='logistic'><Logistics/></div>
            <div>
-                  <Button style ={{ background:'white', color:'black', border:'2px solid black' }}variant="contained" endIcon={<AddShoppingCartIcon />}>
+                  <Button onClick = {()=>addOrder()} style ={{ background:'white', color:'black', border:'2px solid black' }}variant="contained" endIcon={<AddShoppingCartIcon />}>
                     Add Order
                   </Button>
                   <DataTable columns={columns(handleGetProduct)} data = {records} customStyles = {customStyles} pagination  />
@@ -108,7 +153,17 @@ const OrderLink = () =>{
                             <button onClick={() => setOpen(false)}>Close</button>
                     </div>
                   </Popup>
-                
+                  <Popup open ={openOrderPop} onClose={()=>setOpenOrderPop(false)} model nested>
+                    <div className='addorder'>
+                        <h2>Добавление заказ в таблице</h2>
+                        <TextField id="standard-basic" label="Order Status" variant="standard" className = 'text-standard' onChange={(e)=>setOrderStatus(e.target.value)} />
+                        <TextField id="standard-basic" label="Tax Status" variant="standard"  className = 'text-standard' onChange={(e)=>setTaxStatus(e.target.value)}/>
+                        <TextField id="standard-basic" label="Количество" variant="standard"  className = 'text-standard'  onChange={(e)=>setQuantity(e.target.value)}/>
+                        <TextField id="standard-basic" label="Продукт ID" variant="standard"  className = 'text-standard'  onChange={(e)=>setProductId(e.target.value)}/>
+                        <TextField id="standard-basic" label=" Клиент ID" variant="standard"  className = 'text-standard'  onChange={(e)=>setClientID(e.target.value)}/>
+                        <div><button onClick ={()=>PostOrder()}>Добавь</button></div>                        
+                    </div>
+                  </Popup>
            </div>
         </div>
     );
